fef3407262b067cb7a2a1dc93373bbd3d2a884c0
Aaron Stokes <stokescomp01@gmail.com>
2022-03-30 13:41:17 -0600

    add querys for see all, see one, delete one, insert one

--- server.js
+++ server.js
@@ -1,20 +1,60 @@
-const express = require('express');
+const mysql = require("mysql2");
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Express middleware
-app.use(express.urlencoded( { extended: false } ));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    //enter name of your database in the database: "here"
+    database: "",
+  },
+  console.log("connected to the database")
+);
 
+//request to see all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
 
+//request to see a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, rows) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(rows);
+// });
 
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
+//Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
 
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
-
-
+app.use((req, res) => {
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
