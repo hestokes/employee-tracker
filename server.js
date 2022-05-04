48790ea628456b19c355aaed90d83a40799f7958
Aaron Stokes <stokescomp01@gmail.com>
2022-05-04 11:58:43 -0600

    add opening menu

--- server.js
+++ server.js
@@ -19,7 +19,31 @@ connection.connect((err) => {
 });
 
 startAdding = () => {
-  inquirer.prompt([]);
+  inquirer
+    .prompt([
+      {
+        name: "optionMenu",
+        type: "rawlist",
+        message: "Welcome! What is your plan today?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "View all employees by Manger",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee's Role",
+          "Update an employee's Manager",
+          "Remove a department",
+          "Remove a role",
+          "Remove an employee",
+          "View total salaries by department",
+          "Say goodbe for now",
+        ],
+      },
+    ])
+    .then((response) => {});
 };
 
 // const PORT = process.env.PORT || 3001;
