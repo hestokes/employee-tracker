48f625a20645a2bfef101deae86fbee19a517ab4
Aaron Stokes <stokescomp01@gmail.com>
2022-05-04 11:47:44 -0600

    complete schema, start server.js

--- db/schema.sql
+++ db/schema.sql
@@ -5,26 +5,28 @@ USE employeetrackerDB;
 
 
 CREATE TABLE department (
+    
     department_id INTEGER AUTO_INCREMENT PRIMARY KEY, 
     department_name VARCHAR(30)
 );
 
 CREATE TABLE role (
+    
     role_id INTEGER AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(30),
     salary DECIMAL(10,2),
     department_id INTEGER,
-    CONSTRAINT 
+    CONSTRAINT department_fk FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT, 
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    
+    employee_id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id),
-    PRIMARY KEY (id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    manager_id INTEGER NOT NULL,
+    CONSTRAINT role_fk FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE CASCADE,
+    CONSTRAINT manager_fk FOREIGN KEY (manager_id) REFERENCES employee(manager_id) ON DELETE SET NULL
+
 );
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-require('dotenv').config();
+require("dotenv").config();
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const express = require("express");
-require('console.table');
+require("console.table");
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3000,
+  user: "root",
+  password: process.env.password,
+  database: "employeetrackerDB;",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`Connection complete as id ${connection.threadId}`);
+  startAdding();
+});
+
+startAdding = () => {
+  inquirer.prompt([]);
+};
 
 // const PORT = process.env.PORT || 3001;
 // const app = express();
@@ -11,8 +28,6 @@ require('console.table');
 // //Express middleware
 // app.use(express.urlencoded({ extended: false }));
 // app.use(express.json());
-
-
 
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
